beee0197dd460a42a35dff9b6c6413de1b3acad7
chinnarasu-kanthan <crasu341@gmail.com>
2022-02-11 14:55:39 +0530

    3 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+
+const currentUser = null;
+
 import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <BrowserRouter>
+      <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">
+            bezKoder
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+
+            
+
+          
+
+            {currentUser && (
+              <li className="nav-item">
+                <Link to={"/user"} className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
+          </div>
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+        <div className="container mt-3">
+        
+           <Routes>
+              <Route path='/' element={ <Dashboard />} />
+              <Route exact path='/login' element={ <Login />} />
+            </Routes>
+        </div>
+
+        {/* <AuthVerify logOut={logOut}/> */}
+      </div>
+    </BrowserRouter>
   );
 }
